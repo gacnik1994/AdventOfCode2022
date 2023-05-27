5618960d7f7d845f7c3e88d5a7160e9c39272e43
Gacnik <ales.gacnik@gmail.com>
2023-05-27 20:13:45 +0200

    Day1 file update and second answer added to README.md

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -15,7 +15,7 @@ int main() {
             return 1;
             }
         //int for storing highest number
-        int max_calories {0};
+        vector <int> store_calories;
         //int for storing sum for elfs
         int calories {0};
         //buffer for string
@@ -26,10 +26,8 @@ int main() {
             getline(file, line);
             //find empty space in file = end of sum
             if(line.empty()){
-                //compare sum with currently stored highest number
-                if(max_calories < calories){
-                    max_calories = calories;
-                    }
+                //save calories to vector
+                store_calories.push_back(calories);
                 //prepare calories for another sum
                 calories = 0;
             }else{
@@ -38,8 +36,26 @@ int main() {
             }
         }
         //print the highest calorie ammount
-        cout << max_calories << endl;
-        //close file
+        int bubble_loop {0};
+        while (bubble_loop < 3){
+            bubble_loop++;
+            int vector_loop = store_calories.size()-1;
+            while(vector_loop >= 0){
+                if(vector_loop == 0){
+                    break;
+                }else if(store_calories.at(vector_loop) > store_calories.at(vector_loop-1)) {
+                    int a = store_calories.at(vector_loop);
+                    store_calories.at(vector_loop) = store_calories.at(vector_loop-1);
+                    store_calories.at(vector_loop-1) = a;
+                    }
+                vector_loop--;
+            }
+        }
+        int top3 {store_calories.at(0)+store_calories.at(1)+store_calories.at(2)};
+        cout << "Max calories   : " << store_calories.at(0) << endl;
+        cout << "Second highest : " << store_calories.at(1) << endl;
+        cout << "Third highest  : " << store_calories.at(2) << endl;
+        cout << "Sum of top 3 most calories: " << top3 << endl;
         file.close();
         return 0;
 }
